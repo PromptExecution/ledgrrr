672fc874ef593cecb7d1998f89a31c6a532efd8e
elasticdotventures <elasticdotventures@users.noreply.github.com>
2026-08-17 14:03:52 +0000

    deploy: 4f76083690909b2357a51345e2b98cb7f22ed8cc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f28ac4e2.js',
+                path_to_root + 'searchindex-01c0a00f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
